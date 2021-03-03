4effa46b7b2114ab436d395e6831767de3fecdbe
Schimmoellerm <schimmoellerm@gmail.com>
2021-03-03 18:16:22 -0500

    created variables for ajax call of the open weather API

--- Assets/script.js
+++ Assets/script.js
@@ -5,6 +5,12 @@ const openWeatherAPIKey = '0a8a1f2fd6c733eb862c93d061533b6a'
 $('#searchBtn').on("click", function(){
     let input = $('#searchInput').val();
     console.log(input);
+
+    let currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&Appid=" + openWeatherAPIKey + "&units=imperial";
+    console.log(currentWeatherUrl);
+
+    let fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&Appid=" + openWeatherAPIKey + "&units=imperial";
+    console.log(fiveDayUrl);
 });
 
 
