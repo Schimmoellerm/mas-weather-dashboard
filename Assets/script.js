4f0443223153d8b817b92de3be8799775bd2f135
Schimmoellerm <schimmoellerm@gmail.com>
2021-03-03 18:38:10 -0500

    ajax call to get current weather appends to currentWeather div

--- Assets/script.js
+++ Assets/script.js
@@ -11,6 +11,28 @@ $('#searchBtn').on("click", function(){
 
     let fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + input + "&Appid=" + openWeatherAPIKey + "&units=imperial";
     console.log(fiveDayUrl);
+
+    //Current Weather
+    $.ajax({
+        url: currentWeatherUrl,
+        method: "GET"
+    }).then(function (response) {
+        let currentWeather = $("#currentWeather").append("<div>");
+        let currentCity = currentWeather.append('<p>');
+        currentWeather.append(currentCity);
+
+        //Temperature
+        let currentTemp = currentCity.append("<p>");
+        currentCity.append(currentTemp);
+        currentTemp.append("<p>" + "Temperature: " + response.main.temp + "</p>");
+        currentTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
+        currentTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
+    })
+
+
+    //Five Day Forecast
+    
 });
+
 
 
