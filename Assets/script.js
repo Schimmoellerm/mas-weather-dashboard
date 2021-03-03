ccf097ea6e7f0538eeb9b6b5c44586712ef0afb2
Schimmoellerm <schimmoellerm@gmail.com>
2021-03-03 18:10:37 -0500

    tied button to input box with click event in JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,10 @@
 const openWeatherAPIKey = '0a8a1f2fd6c733eb862c93d061533b6a'
+
+/*Search Button************************************************/
+
+$('#searchBtn').on("click", function(){
+    let input = $('#searchInput').val();
+    console.log(input);
+});
+
+
